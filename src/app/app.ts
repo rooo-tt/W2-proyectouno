581b8c8d17c204adbd425dfca837b56d1718f272
rooo-tt <tomasevichro@gmail.com>
2025-09-29 17:12:06 -0300

    cambios en el style

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { About } from './pages/about/about';
-import { Home } from './pages/home/home';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, About, Home],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
