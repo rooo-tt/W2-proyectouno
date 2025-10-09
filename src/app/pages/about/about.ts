d3eebe353257505cb6e610f3df17f0b039f31937
rooo-tt <tomasevichro@gmail.com>
2025-10-09 16:53:23 -0300

    repaso de interfaz y directiva ngfor; aplicamos las unidades de tamaño desde html; aplicamos Modal para ver mas informacion de las tarjetas

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Tarjeta } from '../../models/tarjeta';
 
 @Component({
   selector: 'About',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
+  public infoServicios: Tarjeta[]
+  constructor() {
+    this.infoServicios = [
+      {
+        id: "c1",
+        img: "../../../assets/radiografia.jpg",
+        alt: "radiografia",
+        titulo: "radiografia",
+        descripcion: "Para hacer una radiografía el equipo produce rayos X que atraviesan la parte del cuerpo a examinar.",
+        anualPublicacion: 2025
+      },
+      {
+        id: "c2",
+        img: "../../../assets/ecografia.jpg",
+        alt: "ecografia",
+        titulo: "ecografia",
+        descripcion: "La ecografía es parte importante de los procedimientos de diagnóstico al servir para explorar la mayoría de los órganos del cuerpo.",
+        anualPublicacion: 2025
+      },
+      {
+        id: "c3",
+        img: "../../../assets/laboratorio.jpg",
+        alt: "laboratorio",
+        titulo: "laboratorio",
+        descripcion: "Desde análisis de sangre hasta la observación microscópica, su papel es fundamental en clínica.",
+        anualPublicacion: 2025
+      },
+      {
+        id: "c4",
+        img: "../../../assets/internacion.jpg",
+        alt: "internacion",
+        titulo: "internacion",
+        descripcion: "Es un lugar donde se combina la experiencia médica con el cuidado constante de los pacientes.",
+        anualPublicacion: 2025
+      },
+    ]
+  }
+  /*variable para almacenar tarjeta seleccionada */
+  tarjetaSelecionada: any = null;
 
+  /*recibir el elemento seleccionado por parametro y le asicnamos a "tarjetaSelecionada" */
+  verMas(tarjeta: any) {
+    this.tarjetaSelecionada = tarjeta;
+  }
 }
--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -20,7 +20,7 @@ export class Products {
         titulo: "veterinaria",
         descripcion: "perro con una camisa muy bonita",
         anualPublicacion: 2025
-      }, 
+      },
       {
         id: "t2",
         img: "../../../assets/radiografia.jpg",
@@ -46,5 +46,13 @@ export class Products {
         anualPublicacion: 2025
       }
     ]
+  }
+
+  /*variable para almacenar tarjeta seleccionada */
+  tarjetaSeleccionada: any = null;
+
+  /*recibir el elemento seleccionado por parametro y le asicnamos a "tarjetaSelecionada" */
+  vermasinfo(tarjeta: any) {
+    this.tarjetaSeleccionada = tarjeta;
   }
 }
